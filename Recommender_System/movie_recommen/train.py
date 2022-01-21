a8bc3c34fe1eb545e34e8951b766074750d1cf1d
codecat <2049413359@qq.com>
2022-01-21 13:59:01 +0800

    修改训练脚本

--- Recommender_System/movie_recommen/train.py
+++ Recommender_System/movie_recommen/train.py
@@ -11,8 +11,7 @@ from dataset.dataset import MovieDataset
 
 if __name__ == '__main__':
     dataset = MovieDataset(use_poster=True)
-    usr, mov, score = dataset[0]
-    print(usr)
-    for v in mov:
-        print(v.shape)
-    print(score)
+    print(dataset.max_usr_age)
+    print(dataset.max_usr_job)
+    print(dataset.max_mov_cat)
+    print(dataset.max_mov_tit)
